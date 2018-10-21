d22d6d16e6fb1b2ed1d429cdc92f29ae3d898ca2
Montse Badia <mbadia_11@hotmail.com>
2018-10-21 18:22:14 +0200

    created chats and messages controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :chats, through: :subscriptions
+
+  def existing_chats_users
+    existing_chat_users = []
+    self.chats.each do |chat|
+      existing_chat_users.concat(chat.subscriptions.where.not(user_id: self.id).map {|subscription| subscription.user})
+    end
+    existing_chat_users.uniq
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
   root 'sessions#new'
+  resources :users do
+    resources :chats, only: [:index, :show, :create]
+  end
   resources :sessions, only:[:new, :destroy, :create] 
 end
